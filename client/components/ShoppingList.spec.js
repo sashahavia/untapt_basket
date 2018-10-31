47fb498b4da19172641092e2d0df9eda3b8ad834
Alexandra Havia <sasha.havia@gmail.com>
2018-10-31 19:55:23 -0400

    Client (test): Added test spec for ShoppingList component

---
+++ client/components/ShoppingList.spec.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import React from 'react';
+import enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
+import ShoppingList from './ShoppingList';
+import ListItem from './ListItem';
+
+const adapter = new Adapter();
+enzyme.configure({ adapter });
+
+// create any initial state needed
+const initialState = {};
+// here it is possible to pass in any middleware if needed into //configureStore
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
+describe('Shopping List', () => {
+  let wrapper;
+  let store;
+  let items;
+
+  beforeEach(() => {
+    items = [
+      {
+        name: 'Milk',
+        qty: 1,
+      },
+      {
+        name: 'Bread',
+        qty: 1,
+      },
+    ];
+    store = mockStore(initialState);
+    wrapper = shallow(<ShoppingList store={store} items={items} />).dive();
+  });
+
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  it('renders the heading in an h3', () => {
+    expect(wrapper.find('h3').text()).to.be.equal('Shopping List');
+  });
+
+  it('should render a ul element', () => {
+    expect(wrapper.find('ul').getElement()).to.exist; // eslint-disable-line no-unused-expressions
+  });
+
+  it('renderes ListItem component', () => {
+    expect(wrapper).contains(ListItem);
+  });
+
+  it('renders div with class shopping list', () => {
+    expect(wrapper.exists('.shopping-list')).to.equal(true);
+  });
+});
