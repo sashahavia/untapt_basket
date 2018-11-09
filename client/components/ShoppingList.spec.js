7c36eb90f169561597d213f6f23936c17c3e8236
Alexandra Havia <sasha.havia@gmail.com>
2018-11-08 20:14:19 -0500

    Updated (tests): Implemented mount component in  ShoppingList test specs

--- client/components/ShoppingList.spec.js
+++ client/components/ShoppingList.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import React from 'react';
-import enzyme, { shallow } from 'enzyme';
+import enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
 import ShoppingList from './ShoppingList';
@@ -16,7 +16,7 @@ const middlewares = [];
 const mockStore = configureStore(middlewares);
 
 describe('Shopping List', () => {
-  let wrapper;
+  let wrapper, wrapper2;
   let store;
   let items;
 
@@ -33,6 +33,7 @@ describe('Shopping List', () => {
     ];
     store = mockStore(initialState);
     wrapper = shallow(<ShoppingList store={store} items={items} />).dive();
+    wrapper2 = mount(<ShoppingList store={store} items={items} />);
   });
 
   afterEach(() => {
@@ -53,5 +54,12 @@ describe('Shopping List', () => {
 
   it('renders div with class shopping list', () => {
     expect(wrapper.exists('.shopping-list')).to.equal(true);
+  });
+
+  it('renders list-items', () => {
+    // console.log(wrapper2.debug());
+    // Expect the wrapper object to be defined
+    expect(wrapper2.find('button')).to.have.lengthOf(items.length);
+    expect(wrapper2.find('li')).to.have.lengthOf(items.length);
   });
 });
